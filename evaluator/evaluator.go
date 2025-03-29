19aefc91318736c9eb827075899c34018b89a5e1
danieljuhasz <daniel.juhasz@maesn.com>
2025-03-29 10:32:40 +0100

    return eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -15,7 +15,7 @@ func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 		// Statements
 		case *ast.Program:
-			return evalStatements(node.Statements)
+			return evalProgram(node)
 
 		case *ast.ExpressionStatement:
 			return Eval(node.Expression)
@@ -33,19 +33,26 @@ func Eval(node ast.Node) object.Object {
 			right := Eval(node.Right)
 			return evalInfixExpression(node.Operator, left, right)
 		case *ast.BlockStatement:
-			return evalStatements(node.Statements)
+			return evalBlockStatement(node)
 		case *ast.IfExpression:
 			return evalIfExpression(node)
+		case *ast.ReturnStatement:
+			val := Eval(node.ReturnValue)
+			return &object.ReturnValue{Value: val}
 	}
 
 	return nil
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
-	for _, statement := range stmts {
+	for _, statement := range program.Statements {
 		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
 	}
 
 	return result
@@ -139,6 +146,20 @@ func evalIfExpression(ie *ast.IfExpression) object.Object {
 	} else {
 		return NULL
 	}
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
+	}
+
+	return result
 }
 
 func isTruty(obj object.Object) bool {
