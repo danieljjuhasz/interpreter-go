a0722b90c51797d4ed55f2646fe6018aa9a03dd4
danieljuhasz <daniel.juhasz@maesn.com>
2025-03-29 10:21:09 +0100

    eval if statements

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -32,6 +32,10 @@ func Eval(node ast.Node) object.Object {
 			left := Eval(node.Left)
 			right := Eval(node.Right)
 			return evalInfixExpression(node.Operator, left, right)
+		case *ast.BlockStatement:
+			return evalStatements(node.Statements)
+		case *ast.IfExpression:
+			return evalIfExpression(node)
 	}
 
 	return nil
@@ -122,6 +126,31 @@ func evalIntegerInfixExpression(
 		return nativeBoolToBooleanObject(leftVal != rigthVal)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruty(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruty(obj object.Object) bool {
+	switch obj {
+		case NULL:
+			return false
+		case TRUE:
+			return true
+		case FALSE:
+			return false
+		default:
+			return true
 	}
 }
 
